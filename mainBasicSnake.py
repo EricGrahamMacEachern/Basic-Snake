371c50a0ef489c388f8855e28adcadcc53467d2d
Eric Graham MacEachern <maharg@live.ca>
2015-09-27 20:44:34 -0700

    added the code
    I added the code, license, and readme.

---
+++ mainBasicSnake.py
@@ -0,0 +1,117 @@
+__author__ = 'Eric'
+import pygame
+import random
+
+pygame.init()
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Basic Snake")
+
+block_size = 10
+FPS = 15
+
+font = pygame.font.SysFont(None, 25)
+
+def snake(block_size, snakelist):
+    for XnY in snakelist:
+        pygame.draw.rect(gameDisplay, white, [XnY[0],XnY[1], block_size, block_size])
+
+def message_to_screen(msg, color):
+    screen_text = font.render(msg, True, color);
+    gameDisplay.blit(screen_text, [display_width/2, display_height/2])
+
+def gameLoop():
+
+    clock = pygame.time.Clock()
+
+    gameExit = False
+    gameOver = False
+
+    lead_x = display_width/2
+    lead_y = display_height/2
+    lead_x_change = 0
+    lead_y_change = 0
+
+    snakelist = []
+    snakelength = 1
+
+    randAppleX = round(random.randrange(0, display_width - block_size)/10.0) * 10.0
+    randAppleY = round(random.randrange(0, display_height - block_size)/10.0) * 10.0
+
+    while not gameExit:
+
+        while gameOver == True:
+            gameDisplay.fill(black)
+            message_to_screen("Game over. Press P to play again or Q to quit.", white)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameExit = True
+                    gameOver = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        gameExit = True
+                        gameOver = False
+                    if event.key == pygame.K_p:
+                        gameLoop()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    lead_x_change = -block_size
+                    lead_y_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    lead_x_change = block_size
+                    lead_y_change = 0
+                elif event.key == pygame.K_UP:
+                    lead_y_change = -block_size
+                    lead_x_change = 0
+                elif event.key == pygame.K_DOWN:
+                    lead_y_change = block_size
+                    lead_x_change = 0
+
+        if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y <= 0:
+            gameOver = True
+
+        lead_x += lead_x_change
+        lead_y += lead_y_change
+
+        snakeHead = []
+        snakeHead.append(lead_x)
+        snakeHead.append(lead_y)
+        snakelist.append(snakeHead)
+
+        if len(snakelist) > snakelength:
+            del snakelist[0]
+
+        snake(block_size,snakelist)
+
+        gameDisplay.fill(black)
+        pygame.draw.rect(gameDisplay, white, [randAppleX, randAppleY, block_size, block_size])
+
+        for eachSegment in snakelist[:-1]:
+            if eachSegment == snakeHead:
+                gameOver = True
+
+        snake(block_size, snakelist)
+
+        pygame.display.update()
+
+        if lead_x == randAppleX and lead_y == randAppleY:
+            randAppleX = round(random.randrange(0, display_width - block_size)/10.0) * 10.0
+            randAppleY = round(random.randrange(0, display_height - block_size)/10.0) * 10.0
+            snakelength += 1;
+
+        clock.tick(FPS)
+
+    pygame.quit()
+    quit()
+
+gameLoop()
